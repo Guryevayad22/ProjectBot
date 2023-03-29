483bb18c22fae1a5be0da26ed74db1d55ae6fb2a
Guryevayad22 <Yanagurma04@gmail.com>
2023-03-30 00:31:47 +0300

    Start0

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from aiogram import Bot, types
+from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
 from config import token
 
 bot = Bot(token=token)
